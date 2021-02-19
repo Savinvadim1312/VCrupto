dee10ecee326b0947a2c062d11c721c796b1f881
Vadim Savin <savinvadim1312@gmail.com>
2021-02-19 10:03:25 +0000

    Home screen UI

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
+import styles from './styles';
+const image =  require('../../../assets/images/Saly-1.png');
 
-const HomeScreen = (props) => {
+const HomeScreen = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>HomeScreen component</Text>
+    <View style={styles.root}>
+      <Image style={styles.image} source={image} />
+      <Text style={styles.header1}>Welcome to VCrypto</Text>
+      <Text style={styles.header2}>Invest your virtual $100.000 and compete with others</Text>
     </View>
   );
 };
---
+++ src/screens/HomeScreen/styles.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  root: {
+    padding: 20,
+    alignItems: 'center',
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  image: {
+    height: '40%',
+    aspectRatio: 1,
+  },
+  header1: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 50,
+    marginBottom: 15,
+  },
+  header2: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#707070',
+  }
+});
+
+export default styles;
